a9680ed43b915a9ed57eef9423bba3dea1921805
soz-z <danielbrentclark@gmail.com>
2022-09-04 02:52:46 -0500

    updated changes

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -4,4 +4,8 @@ url = "mongodb+srv://admin:admin@cluster0.ijsqqy5.mongodb.net/pytech"
 client = MongoClient("mongodb+srv://admin:admin@cluster0.ijsqqy5.mongodb.net/pytech")
 db = client.pytech
 
-print(db.list_collection_names)
+
+print("-- Python Collection List --")
+print(db.list_collection_names())
+print("\n")
+input("End of program, press any key to exit... ")
