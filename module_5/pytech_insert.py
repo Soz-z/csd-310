9060df651ca6c8171c1fa152b8e66424bc947237
soz-z <danielbrentclark@gmail.com>
2022-09-04 04:25:59 -0500

    Finished Assignment

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,1 +1,25 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.ijsqqy5.mongodb.net/pytech"
+client = MongoClient("mongodb+srv://admin:admin@cluster0.ijsqqy5.mongodb.net/pytech")
+db = client.pytech
+print("-- INSERT STATEMENTS --")
+def create_student(fname,lname,id=int):
+    student = {
+        "student_id":id,
+        "first_name":fname,
+        "last_name":lname
+    }
+    new_student_id = db.students.insert_one(student).inserted_id
+    print(f"Inserted student record {fname} {lname} into the students collection with document_id", new_student_id)
+
+create_student("Fred","Jackson",1007)
+create_student("Dan", "Smith", 1008)
+create_student("James", "John", 1009)
+
+
+
+print("\n")
+
+input("End of program, press enter to exit... ")
 
--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,1 +1,30 @@
+from pymongo import MongoClient
 
+url = "mongodb+srv://admin:admin@cluster0.ijsqqy5.mongodb.net/pytech"
+client = MongoClient("mongodb+srv://admin:admin@cluster0.ijsqqy5.mongodb.net/pytech")
+db = client.pytech
+
+docs = db.students.find({})
+print(" -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+
+def first_output():
+    for doc in docs:
+        fname = doc["first_name"]
+        lname = doc["last_name"]
+        s_id = doc["student_id"]
+        print(f"Student ID: {s_id}\nFirst Name: {fname}\nLast Name: {lname}\n")
+first_output()
+
+print("-- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY--")
+doc = db.students.find_one({"student_id": 1007})
+
+def second_output(doc):
+    fname = doc["first_name"]
+    lname = doc["last_name"]
+    s_id = doc["student_id"]
+    print(f"Student ID: {s_id}\nFirst Name: {fname}\nLast Name: {lname}\n")
+second_output(doc)
+
+input("End of program, press Enter to continue... ")
+
+# Still unsure on how to code a "press any key to exit...", I know I can make one if the platform is always Windows.
