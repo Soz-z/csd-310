7aaacc11c5a0575c134001997c5e7a190e1bebf1
soz-z <danielbrentclark@gmail.com>
2022-09-11 15:02:33 -0500

    Updated variable

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,11 +1,10 @@
 from pymongo import MongoClient
 
 url = "mongodb+srv://admin:admin@cluster0.ijsqqy5.mongodb.net/pytech"
-client = MongoClient("mongodb+srv://admin:admin@cluster0.ijsqqy5.mongodb.net/pytech")
+client = MongoClient(url)
 db = client.pytech
 
 
 print("-- Python Collection List --")
-print(db.list_collection_names())
-print("\n")
+print(db.list_collection_names(),"\n")
 input("End of program, press any key to exit... ")
