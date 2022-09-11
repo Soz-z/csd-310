80a058575d4a6cfec079246075582eaaba5fbdbb
Soz-z <95555228+Soz-z@users.noreply.github.com>
2022-09-11 15:04:01 -0500

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 url = "mongodb+srv://admin:admin@cluster0.ijsqqy5.mongodb.net/pytech"
-client = MongoClient("mongodb+srv://admin:admin@cluster0.ijsqqy5.mongodb.net/pytech")
+client = MongoClient(url)
 db = client.pytech
 
 
