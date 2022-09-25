1e868c7c0f1ea50a55d3688217b8da1d53dd1630
Soz-z <95555228+Soz-z@users.noreply.github.com>
2022-09-25 00:56:05 -0500

    Update pysports_join_queries.py

--- module_9/pysports_join_queries.py
+++ module_9/pysports_join_queries.py
@@ -1,1 +1,10 @@
+import mysql.connector
+from mysql.connector import errorcode
 
+config = {
+    "user": "pysports_user",
+    "password": "MySQL8IsGreat!",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
